ffd054982fc8f7ba7743e95a006c1c761f9d7d19
grassroadsZ <zys17666541106@163.com>
2021-04-10 13:29:54 +0800

    脚本跑通

---
+++ settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/8 19:41
+# @Author  : grassroadsZ
+# @File    : settings.py
+
+MONGO_DB = {
+    "host": 'localhost',
+    "user": "admin",
+    "pwd": "123456"
+}
+
+API_KEY = ""
+API_SECRET = ""
+
+
