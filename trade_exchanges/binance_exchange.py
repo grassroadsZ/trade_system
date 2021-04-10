d100ebd37a0a621576b3ee25ff51741888bd0663
grassroadsZ <zys17666541106@163.com>
2021-04-10 14:24:22 +0800

    本地调试

--- trade_exchanges/binance_exchange.py
+++ trade_exchanges/binance_exchange.py
@@ -5,5 +5,9 @@ from settings import API_KEY, API_SECRET
 exchange = ccxt.binance({
     'apiKey': API_KEY,
     'secret': API_SECRET,
-    'enableRateLimit': True
+    'enableRateLimit': True,
+    "verbose":True,
 })
+# print(exchange.options)
+
+print(exchange.public_get_ticker_price(params={"symbol":"BTCUSDT"}))
